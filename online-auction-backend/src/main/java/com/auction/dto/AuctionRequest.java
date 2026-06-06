4754ac5dfed7c563d1db3996bbaf70226d36f768
gaurav-mandage-1718 <gauravmandage64@gmail.com>
2026-06-06 19:44:19 +0530

    add update auction creation and management APIs

---
+++ online-auction-backend/src/main/java/com/auction/dto/AuctionRequest.java
@@ -0,0 +1,44 @@
+package com.auction.dto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class AuctionRequest {
+
+    private Long productId;
+    private BigDecimal startingPrice;
+    private BigDecimal buyNowPrice;
+    private LocalDateTime endTime;
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(Long productId) {
+        this.productId = productId;
+    }
+
+    public BigDecimal getStartingPrice() {
+        return startingPrice;
+    }
+
+    public void setStartingPrice(BigDecimal startingPrice) {
+        this.startingPrice = startingPrice;
+    }
+
+    public BigDecimal getBuyNowPrice() {
+        return buyNowPrice;
+    }
+
+    public void setBuyNowPrice(BigDecimal buyNowPrice) {
+        this.buyNowPrice = buyNowPrice;
+    }
+
+    public LocalDateTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalDateTime endTime) {
+        this.endTime = endTime;
+    }
+}
